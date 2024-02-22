ae3475b1fe1335441163c3b69a5ebcdb6e53c642
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2024-02-22 01:41:38 -0500

    Change back to stock

--- README.md
+++
@@ -1,2 +0,0 @@
-# kernel_samsung_gteslte
- Kernel for Galaxy Tab E 8.0 (SM-T377W) / GTESLTEDMC
--- build.sh
+++
@@ -1,9 +0,0 @@
-export ARCH=arm && export SUBARCH=arm
-
-export CROSS_COMPILE=$HOME/android/Workspace/gtesltebmc/prebuilts/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
-
-make clean
-make mrproper
-make gtesltecan_00_defconfig
-## make -j1
-make -j$(nproc --all)
