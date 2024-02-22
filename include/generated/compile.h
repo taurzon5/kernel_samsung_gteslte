def22e48dade3de68a392ecf0b403f83075476ec
Khrystan Taur <73740076+taurzon5@users.noreply.github.com>
2024-02-22 01:32:00 -0500

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Feb 22 00:07:59 EST 2024"
+#define LINUX_COMPILE_BY "taurzon5"
+#define LINUX_COMPILE_HOST "taurzon5-OMEN-Laptop-15-ek0018tx"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
